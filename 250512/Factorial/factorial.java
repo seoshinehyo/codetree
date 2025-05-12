77b639484bf20cc420438d3394d579ab98bd8829
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-05-12 15:17:11 +0900

    Title: Factorial, Time: 139ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250512/Factorial/factorial.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+}
