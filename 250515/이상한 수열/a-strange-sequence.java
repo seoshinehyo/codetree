85d54d035e5a3b3489862fde917f2d725fe8aec5
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-05-15 13:40:15 +0900

    Title: 이상한 수열, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250515/이상한 수열/a-strange-sequence.java
+++ 250515/이상한 수열/a-strange-sequence.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        arr = new int[15];
+        arr = new int[n + 1];
         System.out.print(getN(n));
     }
 
