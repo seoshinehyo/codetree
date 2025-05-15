81613e548c2c57a633f5764364b15e0a9d70b50c
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-05-15 13:39:21 +0900

    Title: 이상한 수열, Time: 142ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250515/이상한 수열/a-strange-sequence.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+
+    static int[] arr;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        arr = new int[15];
+        System.out.print(getN(n));
+    }
+
+    public static int getN(int n) {
+        if (n == 1) {
+            return arr[n] = 1;
+        }
+        if (n == 2) {
+            return arr[n] = 2;
+        }
+        if (arr[n] != 0) {
+            return 0;
+        }
+        arr[n] = getN(n / 3) + getN(n - 1);
+        return arr[n];
+    }
+}
