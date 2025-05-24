641745369101afd993e768e2ddc4b78b0b3e0237
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-05-24 12:51:38 +0900

    Title: 중앙값 계산 2, Time: 86ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250524/중앙값 계산 2/get-median-2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (i % 2 == 0) {
+                Arrays.sort(arr, 0, i + 1);
+                System.out.print(arr[i / 2] + " ");
+            }
+        }
+    }
+}
