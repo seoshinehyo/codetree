97abe4f17d7c453f0bd007f64fe8d49b372cb849
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-06-24 14:57:09 +0900

    Title: Date to Date, Time: 102ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250624/Date to Date/date-to-date.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] num_of_days = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int m1 = Integer.parseInt(st.nextToken());
+        int d1 = Integer.parseInt(st.nextToken());
+        int m2 = Integer.parseInt(st.nextToken());
+        int d2 = Integer.parseInt(st.nextToken());
+
+        System.out.print(d2 + (num_of_days[m1] - d1 + 1));
+    }
+}
