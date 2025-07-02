65b50bd18d9c935cb5d9060b59269614733ef000
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-07-02 23:31:30 +0900

    Title: 최대로 겹치는 구간, Time: 96ms, Memory: 9MB, Status: Passed - Codetree

--- 250702/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 250702/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -5,24 +5,26 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[101];
+        final int OFFSET = 100;
+        final int SIZE = 201;
+        int[] arr = new int[SIZE];
 
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
             int x2 = Integer.parseInt(st.nextToken());
-
+            
             for (int j = x1; j < x2; j++) {
-                arr[j] += 1;
+                arr[j + OFFSET]++;
             }
         }
 
         int max = 0;
-
-        for (int i = 0; i < 101; i++) {
-            if (max < arr[i]) {
-                max = arr[i];
+        for (int count : arr) {
+            if (count > max) {
+                max = count;
             }
         }
 
