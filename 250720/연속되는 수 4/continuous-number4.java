067c209553ee5d307b1f979e2dd3056ccc210fad
Sanghyo Seo <125882656+seoshinehyo@users.noreply.github.com>
2025-07-20 23:27:40 +0900

    Title: 연속되는 수 4, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250720/연속되는 수 4/continuous-number4.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        final int SIZE = 1_001;
+        int[] arr = new int[SIZE];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 1;
+        int max = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) {
+                count++;
+            } else {
+                max = Math.max(max, count);
+                count = 1;
+            }
+        }
+
+        max = Math.max(max, count);
+        System.out.println(max);
+    }
+}
